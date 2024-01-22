1d47fe3354f98f5933e7351319db98ddaeedcbc1
danhalf <danhel@rocketmail.com>
2024-01-22 23:59:45 +0200

    added: input

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,16 +29,38 @@
         private void InitializeComponent()
         {
             add_data_button = new Button();
+            label_info = new Label();
+            greatings_input = new TextBox();
             SuspendLayout();
             // 
             // add_data_button
             // 
             add_data_button.Location = new Point(357, 316);
             add_data_button.Name = "add_data_button";
-            add_data_button.Size = new Size(75, 23);
+            add_data_button.Size = new Size(100, 23);
             add_data_button.TabIndex = 0;
             add_data_button.Text = "Add data";
             add_data_button.UseVisualStyleBackColor = true;
+            add_data_button.Click += add_data_button_Click;
+            // 
+            // label_info
+            // 
+            label_info.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label_info.Location = new Point(357, 373);
+            label_info.Name = "label_info";
+            label_info.Size = new Size(100, 24);
+            label_info.TabIndex = 1;
+            label_info.TextAlign = ContentAlignment.MiddleCenter;
+            label_info.Click += label_info_Click;
+            // 
+            // greatings_input
+            // 
+            greatings_input.Location = new Point(306, 160);
+            greatings_input.Name = "greatings_input";
+            greatings_input.Size = new Size(206, 23);
+            greatings_input.TabIndex = 2;
+            greatings_input.WordWrap = false;
+            greatings_input.TextChanged += greatings_input_TextChanged;
             // 
             // Form1
             // 
@@ -46,14 +68,19 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.GradientInactiveCaption;
             ClientSize = new Size(800, 510);
+            Controls.Add(greatings_input);
+            Controls.Add(label_info);
             Controls.Add(add_data_button);
             Name = "Form1";
             Text = "WinFormPro";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button add_data_button;
+        private Label label_info;
+        private TextBox greatings_input;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,8 +5,33 @@ namespace WinFormPro
         public Form1()
         {
             InitializeComponent();
+
+            AutoCompleteStringCollection collection = new AutoCompleteStringCollection()
+            {
+                "Username",
+                "Userpassword",
+                "Union"
+            };
+
+            greatings_input.AutoCompleteCustomSource = collection;
+            greatings_input.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            greatings_input.AutoCompleteSource = AutoCompleteSource.CustomSource;
         }
 
+        private void add_data_button_Click(object sender, EventArgs e)
+        {
+            string labeltText = greatings_input.Text;
+            label_info.Text = "Hello, " + labeltText;
+        }
 
+        private void greatings_input_TextChanged(object sender, EventArgs e)
+        {
+
+            label_info.Text = greatings_input.Text;
+        }
+
+        private void label_info_Click(object sender, EventArgs e)
+        {
+        }
     }
 }
